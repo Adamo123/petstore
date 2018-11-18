7bfae6d6896042c6a1b61fdf3675b51e1ba7c8af
Adamo123 <adamosacchetti@outlook.com>
2018-11-18 10:26:30 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function CallIt() {
+	alert("What the heck!!"); 
+	prompt("Hello" , "What's your name?" )
+}
+
+/* function CallItAgain() {
+	alert("You moused over the div!!");
+
+
+}
+
+*/
+
+
+// var message = "Hello";
+function Again(message) {
+	alert("This is your message: " + message);
+
+}
+
+/*
+
+function load() {
+
+	alert("The page is loading " + new Date());
+
+
+
+}
+
+*/
